9c68bd3a0be8939b3416d311485fd690ac3e1e11
Chance Tarver <tarver.chance@gmail.com>
2022-02-08 09:53:08 -0600

    Update channel to broadcast < 4096 per time.

--- MATLAB/aclr_test.m
+++ MATLAB/aclr_test.m
@@ -1,6 +1,9 @@
 %% Settings.
+clear;clc;close all;
 p.n_users = 1;
 p.n_ants = 16;
+
+sim_mode = 1;
 
 p.users.distance = 10;
 p.users.theta = 100;
@@ -9,52 +12,54 @@ p.mod.name = 'OFDM';
 p.mod.required_domain = 'freq';
 p.mod.required_fs = 7.68e6;
 p.mod.n_users = p.n_users;
-p.mod.n_scs = 200;
+p.mod.n_scs = 100;
 p.mod.fft_size = 512;
-p.mod.n_symbols = 14;
+p.mod.n_symbols = 2;
 p.mod.make_cyclic = true;
 p.mod.use_windowing = true;
-p.mod.window_length = 64;
+p.mod.window_length = 32;
 p.mod.use_random = false;
 
-p.bs_array.name = 'Sim_Array'; % or 'ARGOS';
-p.bs_array.n_antennas = p.n_ants;
-p.bs_array.required_fs = p.mod.required_fs;
-p.bs_array.required_domain = 'time';
-
-p.bs_array.name = 'IRIS';
-p.bs_array.n_antennas = p.n_ants;
-p.bs_array.required_fs = p.mod.required_fs;
-p.bs_array.required_domain = 'time';
-p.bs_array.use_hub = true;
-p.bs_array.wired_ue = false;
-p.bs_array.tx_freq = 3.6e9;  
-p.bs_array.rx_freq = 3.6e9;
-p.bs_array.tx_gain = 75;
-p.bs_array.rx_gain = 60;
-p.bs_array.chain_ids = 2:3;
-p.bs_array.node_ids = 1:8;
-p.bs_array.sched = "BGPG";
-
-p.ue_array.name = 'Sim_Array';
-p.ue_array.n_antennas = p.n_users;
-p.ue_array.required_fs = p.mod.required_fs;
-p.ue_array.required_domain = 'freq';
-
-p.ue_array.name = 'IRIS';
-p.ue_array.n_antennas = 1;
-p.ue_array.required_fs = p.mod.required_fs;
-p.ue_array.required_domain = 'time';
-p.ue_array.wired_ue = false;
-p.ue_array.use_hub = false;
-p.ue_array.tx_freq = 3.6e9;  
-p.ue_array.rx_freq = 3.6e9;
-p.ue_array.tx_gain = 75;
-p.ue_array.rx_gain = 60;
-p.ue_array.chain_ids = 7;
-p.ue_array.node_ids = 1;
-p.ue_array.sched = "GGRG";
-
+if sim_mode
+    p.bs_array.name = 'Sim_Array'; % or 'ARGOS';
+    p.bs_array.n_antennas = p.n_ants;
+    p.bs_array.required_fs = p.mod.required_fs;
+    p.bs_array.required_domain = 'time';
+else
+    p.bs_array.name = 'IRIS';
+    p.bs_array.n_antennas = p.n_ants;
+    p.bs_array.required_fs = p.mod.required_fs;
+    p.bs_array.required_domain = 'time';
+    p.bs_array.use_hub = true;
+    p.bs_array.wired_ue = false;
+    p.bs_array.tx_freq = 3.6e9;
+    p.bs_array.rx_freq = 3.6e9;
+    p.bs_array.tx_gain = 75;
+    p.bs_array.rx_gain = 60;
+    p.bs_array.chain_ids = 2:3;
+    p.bs_array.node_ids = 1:8;
+    p.bs_array.sched = "BGPG";
+end
+if sim_mode
+    p.ue_array.name = 'Sim_Array';
+    p.ue_array.n_antennas = p.n_users;
+    p.ue_array.required_fs = p.mod.required_fs;
+    p.ue_array.required_domain = 'freq';
+else
+    p.ue_array.name = 'IRIS';
+    p.ue_array.n_antennas = 1;
+    p.ue_array.required_fs = p.mod.required_fs;
+    p.ue_array.required_domain = 'time';
+    p.ue_array.wired_ue = false;
+    p.ue_array.use_hub = false;
+    p.ue_array.tx_freq = 3.6e9;
+    p.ue_array.rx_freq = 3.6e9;
+    p.ue_array.tx_gain = 75;
+    p.ue_array.rx_gain = 60;
+    p.ue_array.chain_ids = 7;
+    p.ue_array.node_ids = 1;
+    p.ue_array.sched = "GGRG";
+end
 % Only used for simulation arrays.
 p.sim_channel.name = 'Quadriga';
 p.sim_channel.required_fs = p.mod.required_fs;
@@ -63,12 +68,13 @@ p.sim_channel.n_ant = p.n_ants;
 p.sim_channel.n_users = p.n_users;
 p.sim_channel.fft_size =  p.mod.fft_size;
 
-% Real channel that we will learn. 
+% Real channel that we will learn.
 p.real_channel.name = 'RealChannel';
 p.real_channel.required_fs = p.mod.required_fs;
 p.real_channel.required_domain = 'freq';
 p.real_channel.n_scs = p.mod.fft_size;
 p.real_channel.n_ants = p.n_ants;
+p.real_channel.one_shot = false;
 
 % Precoder.
 p.precoder.name = 'MRT';
